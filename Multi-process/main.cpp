a559f32c68ddad52d559d8f6f44543983328ade1
vincent chung <vincentcw.chung@gmail.com>
2016-08-26 23:51:03 +0800

    Initial Commit

---
+++ Multi-process/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Multi-process
+//
+//  Created by 鐘淳文 on 2016/8/26.
+//  Copyright © 2016年 vincent. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
